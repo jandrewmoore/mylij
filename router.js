a221d8e2814ede66f8d5cddb53ccc58c65f1c73e
Andrew Moore <jandrewmoore@me.com>
2017-08-05 20:26:35 -0400

    pass collection to page views correctly
    forgot to prepend "this."

--- router.js
+++ router.js
@@ -12,28 +12,31 @@ const MylijRouter = Backbone.Router.extend({
   },
 
   loadHomePage: function () {
-    const home = new Home({collection: collection});
+    const home = new Home({collection: this.collection});
     $('#root').html(home.render().el);
 
     console.log('Load home page')
   },
 
   loadAddPage: function () {
-    const add = new Add({collection: collection})
+    const add = new Add({
+      collection: this.collection,
+    })
+    
     $('#root').html(add.render().el);
 
     console.log('Load add page')
   },
 
   loadEntriesPage: function () {
-    const entries = new Entries({collection: collection})
+    const entries = new Entries({collection: this.collection})
     $('#root').html(entries.render().el);
 
     console.log('Load entries page')
   },
 
   loadVisualsPage: function () {
-    const visuals = new Visuals({collection: collection})
+    const visuals = new Visuals({collection: this.collection})
     $('#root').html(visuals.render().el);
 
     console.log('Load visuals page')
